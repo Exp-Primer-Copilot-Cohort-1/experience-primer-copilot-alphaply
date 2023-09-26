28f25cb9609c9caf36d2d5fb1c4ca27b8ba66a60
alphaply <1526147838@qq.com>
2023-09-26 09:24:16 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,110 +1,80 @@
-//create web server
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var Comment = require('./models/comment');
-var Post = require('./models/post');
-var db = mongoose.connection;
-var ObjectId = require('mongodb').ObjectID;
-var cors = require('cors');
-var path = require('path');
-var port = process.env.PORT || 3000;
-var router = express.Router();
-var corsOptions = {
-  origin: 'http://localhost:4200',
-  optionsSuccessStatus: 200
-};
-//connect to database
-mongoose.connect('mongodb://localhost:27017/comments');
-//check connection
-db.once('open', function(){
-  console.log('Connected to MongoDB');
-});
-//check for db errors
-db.on('error', function(err){
-  console.log(err);
-});
-//set view engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-//set public folder
-app.use(express.static(path.join(__dirname, 'public')));
-//body parser middleware
+// Create web server
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+
+// Create express app
+const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-//enable cors
-app.use(cors(corsOptions));
-//get comments
-app.get('/comments', function(req, res, next){
-  Comment.find({}, function(err, comments){
-    if(err){
-      console.log(err);
-    } else {
-      res.json(comments);
-    }
+app.use(cors());
+
+// Create a comments object
+const commentsByPostId = {};
+
+// Create a get route to get the comments for a specific post
+app.get('/posts/:id/comments', (req, res) => {
+  // Return the comments object for the given post id
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+// Create a post route to post a comment to a specific post
+app.post('/posts/:id/comments', (req, res) => {
+  // Create a random id for the comment
+  const commentId = randomBytes(4).toString('hex');
+  // Get the comment content from the request body
+  const { content } = req.body;
+  // Get the comments for the given post id
+  const comments = commentsByPostId[req.params.id] || [];
+  // Push the new comment into the comments array
+  comments.push({ id: commentId, content, status: 'pending' });
+  // Set the comments for the given post id
+  commentsByPostId[req.params.id] = comments;
+  // Emit an event to the event bus containing the id, content, and status of the comment
+  axios.post('http://event-bus-srv:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+      status: 'pending',
+    },
   });
+  // Return the comments array
+  res.status(201).send(comments);
 });
-//get single comment
-app.get('/comments/:id', function(req, res, next){
-  Comment.findById(req.params.id, function(err, comment){
-    if(err){
-      console.log(err);
-    } else {
-      res.json(comment);
+
+// Create a post route to handle an event
+app.post('/events', async (req, res) => {
+  // Get the event type and data from the request body
+  const { type, data } = req.body;
+  // If the event type is CommentModerated
+  if (type === 'CommentModerated') {
+    // Get the comments for the given post id
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+    // Find the comment with the given id
+    const comment = comments.find((comment) => {
+      return comment.id === id;
     }
-  });
+    );
+    // Set the status of the comment to the status from the event data  
+    comment.status = status;
+    // Emit an event to the event bus containing the id, content, status, and postId of the comment
+    await axios.post('http://event-bus-srv:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        content,
+        postId,
+        status,
+      },
+    });
+  }
+  // Send a response
+  res.send({});
+
 });
-//add comment
-app.post('/comments/add', function(req, res, next){
-  console.log(req.body);
-  let comment = new Comment();
-  comment.username = req.body.username;
-  comment.title = req.body.title;
-  comment.body = req.body.body;
-  comment.save(function(err){
-    if(err){
-      console.log(err);
-    } else {
-      res.json({message: 'Comment Added'});
-    }
-  });
-});
-//update comment
-app.put('/comments/:id', function(req, res, next){
-  let query = {_id: req.params.id}
-  let comment = new Comment();
-  comment.username = req.body.username;
-  comment.title = req.body.title;
-  comment.body = req.body.body;
-  Comment.update(query, comment, function(err){
-    if(err){
-      console.log(err);
-    } else {
-      res.json({message: 'Comment Updated'});
-    }
-  });
-});
-//delete comment
-app.delete('/comments/:id', function(req, res, next){
-  let query = {_id: req.params.id}
-  Comment.remove(query, function(err){
-    if(err){
-      console.log(err);
-    }
-    res.json({message: 'Comment Removed'});
-  });
-});
-//get posts
-app.get('/posts', function(req, res, next){
-  Post.find({}, function(err, posts){
-    if(err){
-      console.log(err);
-    } else {
-      resdb.once('open', function(){
-  console.log('Connected to MongoDB');
-});
-//check for db errors
-db.on('error', function(err){
-  console.log(err);
-});
+
+
+
